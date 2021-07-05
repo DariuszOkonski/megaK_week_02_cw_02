c73bb788d8401d933cdd1845fb471c2c65dbade2
Dariusz Okonski <darek200180@gmail.com>
2021-07-05 13:01:32 +0100

    finished task

--- script.js
+++ script.js
@@ -5,4 +5,46 @@ const checkIfCorrectNumberType = (someArgument) => {
     return true;
 }
 
-console.log("hello world");
+const convertStringToNumber = () => {
+    let input = prompt(`Provide number: `)
+    while(!checkIfCorrectNumberType(input)){
+        input = prompt(`Incorrect "${input}" number, try again: `)
+    }
+    return parseInt(Math.round(input));
+}
+
+const checkNumber = (number) => {
+    if(number % 2 === 0)
+        return even(number);
+    return odd(number)
+}
+
+const even = (number) => {
+    return number;
+}
+
+const odd = (number) => {
+    return number * 2;
+}
+
+const sendResponseToConsole = (number, response) => {
+    console.log(`Given Number: ${number}`);
+    console.log(`Response: ${response}`)
+}
+
+const sendResponseToHTML = (number, response) => {
+    document.body.innerHTML = `        
+        <p>Given Number: ${number}</p>
+        <p>Response: ${response}</p>
+    `;
+};
+
+const main = () => {
+    const number = convertStringToNumber();
+    const response = checkNumber(number);
+
+    sendResponseToConsole(number, response);
+    sendResponseToHTML(number, response);
+}
+
+main();
